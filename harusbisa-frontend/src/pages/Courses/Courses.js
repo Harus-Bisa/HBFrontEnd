23bc8cbdaae8883bf8809a87d44f76b558899c44
Danika Widjaja <danika.dew@utexas.edu>
2019-11-08 00:13:26 -0600

    course card impl

--- harusbisa-frontend/src/pages/Courses/Courses.js
+++ harusbisa-frontend/src/pages/Courses/Courses.js
@@ -3,11 +3,12 @@ import { withAuth } from "../withAuth";
 import services from "../../Services";
 import {connect} from "react-redux";
 import { getCourses } from "../../redux/actions";
+import { ProfCourseCard } from "../../components/Card/Card";
 
 function mapStateToProps(state){
     if (state){
         return{
-            data: state.data,
+            courses: state.courses,
             loading: false
         };
     }
@@ -18,20 +19,37 @@ function mapStateToProps(state){
 }
 
 function Courses(props){
+    useEffect(() => {
+        props.getCourses();
+    })
+
     const logout = () =>{
         services.logout();
         props.history.push("/")
     }
-    useEffect(() => {
-        props.getCourses();
-    })
+
+    
+    const makeCards = () => {
+        var courses = props.courses;
+        var cards = []
+        for (var i=0; i<courses.length; i++){
+            var course = courses[i];
+            var cardProps = {
+                title: course.course_name,
+                link: "/dashboard/"+course._id 
+            }
+            cards.push(<ProfCourseCard key={i} content={cardProps}/>)
+        }
+        return cards
+    };
     if (props.loading){
         return(<p>Loading</p>)
     }
+
     return(
         <div>
             Courses
-            <p>{props.data.email}</p>
+            {makeCards()}
             <button onClick={logout}>logout</button>
         </div>
     )
