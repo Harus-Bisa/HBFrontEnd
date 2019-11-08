da9f8e16dabaa273b04bec58e277e04fbd5b5fef
Danika Widjaja <danika.dew@utexas.edu>
2019-11-08 00:12:45 -0600

    adding dashboard route

--- harusbisa-frontend/src/App.js
+++ harusbisa-frontend/src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch, withRouter} from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Courses from './pages/Courses/Courses';
 import './App.css';
+import ProfDashboard from './pages/Dashboard/ProfDashboard';
 
 function App() {
   return (
       <Switch>
         <Route exact path="/" component={Login}/>
         <Route exact path="/courses" component={Courses}/>
+        <Route exact path="/dashboard/:id" component={ProfDashboard}/>
       </Switch>
   );
 }
