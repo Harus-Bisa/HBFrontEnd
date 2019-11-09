74bc31dbe5ddd11af63527d6a2108fa43c4d7c61
Danika Widjaja <danika.dew@utexas.edu>
2019-11-09 17:21:14 -0600

    adding additional props to propComponent

--- harusbisa-frontend/src/components/Popup/Popup.js
+++ harusbisa-frontend/src/components/Popup/Popup.js
@@ -3,13 +3,15 @@ import { Button, Dialog, DialogContent } from "@material-ui/core";
 
 export default function Popup(props){
     const [open = false, setOpen] = React.useState();
-    
+    const ContentComponent = props.content
 
     return(
         <div>
             <Button onClick={() => setOpen(true)}>{props.purpose}</Button>
             <Dialog open={open} onClose={() => setOpen(false)}>
-                <DialogContent>{props.content}</DialogContent>
+                <DialogContent>
+                    <ContentComponent closePopup={() => setOpen(false)}/>
+                </DialogContent>
             </Dialog>
         </div>
     )
