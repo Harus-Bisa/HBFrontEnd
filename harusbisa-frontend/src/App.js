81c00597511ab9369e2c33221ea246c27c163348
Danika Widjaja <danika.dew@utexas.edu>
2019-11-10 23:19:56 -0600

    routing update

--- harusbisa-frontend/src/App.js
+++ harusbisa-frontend/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Route, Switch, withRouter} from "react-router-dom";
 import Login from "./pages/Login/Login";
-import Courses from './pages/Courses/Courses';
+import FacultyCourses from './pages/Professor/Courses/Courses';
+import StudentCourses from "./pages/Student/Courses/Courses";
 import './App.css';
-import ProfDashboard from './pages/Dashboard/ProfDashboard';
+import ProfDashboard from './pages/Professor/Dashboard/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
       <Switch>
         <Route exact path="/" component={Login}/>
-        <Route exact path="/courses" component={Courses}/>
-        <Route exact path="/dashboard/:id" component={ProfDashboard}/>
+        <Route exact path="/faculty/courses" component={FacultyCourses}/>
+        <Route exact path="/faculty/dashboard/:id" component={ProfDashboard}/>
+
+        <Route exact path="/student/courses" component={StudentCourses}/>
       </Switch>
   );
 }
