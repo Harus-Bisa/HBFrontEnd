87bd9210f4c81c503cd15a79c6f3ea74f789be46
Danika Widjaja <danika.dew@utexas.edu>
2019-11-11 23:27:54 -0600

    edit student course card

--- harusbisa-frontend/src/components/Card/StudentCourseCard.js
+++ harusbisa-frontend/src/components/Card/StudentCourseCard.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Card, CardContent, CardActions, Button } from "@material-ui/core";
+import { Card, CardContent, CardActions, CardHeader, IconButton } from "@material-ui/core";
 import {connect} from "react-redux";
-import {deleteCourse} from "../../redux/actions";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 import "../../css/card.css";
 
 function mapStateToProps(state, currentProps){
@@ -17,19 +17,23 @@ function mapStateToProps(state, currentProps){
     return null
 }
 function StudentCourseCard(props){
-    const deleteCourse = () =>{
-        props.deleteCourse(props.course._id)   
-    }
     return(
         <div className="col-md-6">
             <Card className="card student-card">
+                <CardHeader
+                    action={
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon/>
+                        </IconButton>
+                    }
+                    title={<h5><a href={"/student/dashboard/"+props.course._id}>{props.course.course_name}</a></h5>}
+                />
                 <CardContent>
-                    <h5><a href={"/student/dashboard/"+props.course._id}>{props.course.course_name}</a></h5>
+                    {/* <br/> */}
                     <p>{props.course.start_term} - {props.course.end_term}</p>
                     <p>{props.course.instructor}</p>
                 </CardContent>
                 <CardActions className="action">
-                    {/* <Button onClick={deleteCourse}>Delete</Button> */}
                     <p>{props.course.number_of_lectures} Sesi</p>
                 </CardActions>
             </Card>
@@ -37,4 +41,4 @@ function StudentCourseCard(props){
     )
 }
 
-export default connect(mapStateToProps, {deleteCourse})(StudentCourseCard);
+export default connect(mapStateToProps)(StudentCourseCard);
