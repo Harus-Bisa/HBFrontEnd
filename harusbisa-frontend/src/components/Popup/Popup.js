6659c1be32e6584a0259f5c33080b9786df41980
Danika Widjaja <danika.dew@utexas.edu>
2019-11-14 13:48:00 -0600

    popup error handling

--- harusbisa-frontend/src/components/Popup/Popup.js
+++ harusbisa-frontend/src/components/Popup/Popup.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Dialog, DialogContent, Button } from "@material-ui/core";
 import CourseForm from "../Form/CourseForm";
+import { removeError } from "../../redux/actions";
+import {connect} from "react-redux";
 
-export default function Popup(props){
+function Popup(props){
     const [open, setOpen] = React.useState(false);
     const ContentComponent = props.content
     const Trigger = props.trigger.component
+    
+    const openPopup = () =>{
+        setOpen(true)
+        props.removeError()
+    }
     return(
         <div>
-            <Trigger className={props.trigger.className} onClick={() => setOpen(true)}>{props.purpose}</Trigger>
+            <Trigger className={props.trigger.className} onClick={openPopup}>{props.purpose}</Trigger>
             <Dialog open={open} onClose={() => setOpen(false)} style={{padding:0}}>
                 <DialogContent> 
                     <ContentComponent closePopup={() => setOpen(false)} id={props.id}/>
@@ -25,3 +32,5 @@ Popup.defaultProps={
     },
     content: CourseForm
 }
+
+export default connect(null, {removeError})(Popup)
