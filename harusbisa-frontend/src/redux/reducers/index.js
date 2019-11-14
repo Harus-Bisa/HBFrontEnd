faad9f9b993207412de78ec25676286b8fdcb6bc
Danika Widjaja <danika.dew@utexas.edu>
2019-11-14 13:47:52 -0600

    redux error and studentaddcourse  handling

--- harusbisa-frontend/src/redux/reducers/index.js
+++ harusbisa-frontend/src/redux/reducers/index.js
@@ -1,4 +1,4 @@
-import { COURSES_LOADED, COURSE_LOADED, CHANGE_SELECTED_LECTURE, ADD_COURSE, DELETE_COURSE, EDIT_COURSE, USER_LOADED } from "../constants/action-types";
+import { COURSES_LOADED, COURSE_LOADED, CHANGE_SELECTED_LECTURE, ADD_COURSE, DELETE_COURSE, EDIT_COURSE, USER_LOADED, STUDENT_ADD_COURSE, ERROR, REMOVE_ERROR } from "../constants/action-types";
 
 function rootReducer(state ={loading: true}, action){
     if (action.type === COURSES_LOADED){
@@ -43,6 +43,23 @@ function rootReducer(state ={loading: true}, action){
             firstName: action.payload.firstName,
             lastName: action.payload.lastName,
             role: action.payload.role
+        })
+    }
+    if (action.type === STUDENT_ADD_COURSE){
+        return Object.assign({}, state, {
+            courses: action.payload,
+            loading: false,
+        }); 
+    }
+    if(action.type === ERROR){
+        console.log('error')
+        return Object.assign({}, state, {
+            error: action.payload
+        })
+    }
+    if(action.type === REMOVE_ERROR){
+        return Object.assign({}, state, {
+            error: null
         })
     }
     return state;
