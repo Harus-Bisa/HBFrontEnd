98327a94b0c2d253668e2c669a67f41246dffee8
Danika Widjaja <danika.dew@utexas.edu>
2019-11-14 22:18:46 -0600

    iconbutton encapsulation

---
+++ harusbisa-frontend/src/components/ButtonIcon/ButtonIcon.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { IconButton } from "@material-ui/core";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+export default function ButtonIcon(props){
+    const Icon = props.icon;
+    return(
+        <IconButton className={props.className} onClick={props.onClick}>
+            <Icon/>
+        </IconButton>
+    )
+}
+
+ButtonIcon.defaultProps={
+    className:"",
+    icon:MoreVertIcon,
+    onClick:(()=>{})
+}
