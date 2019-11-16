70299e3c34e6dab388b55d97cb572d8f20c4431a
Danika Widjaja <danika.dew@utexas.edu>
2019-11-16 14:57:41 -0600

    login and logout in redux

--- harusbisa-frontend/src/redux/constants/action-types.js
+++ harusbisa-frontend/src/redux/constants/action-types.js
@@ -6,8 +6,13 @@ export const DELETE_COURSE = "DELETE_COURSE";
 export const EDIT_COURSE = "EDIT_COURSE";
 export const USER_LOADED = "USER_LOADED";
 
-/*STUDENT */
+/* STUDENT */
 export const STUDENT_ADD_COURSE = "STUDENT_ADD_COURSE";
 
+/* ERROR */
 export const ERROR = "ERROR";
 export const REMOVE_ERROR = "REMOVE_ERROR";
+
+/* USER */
+export const LOG_IN = "LOG_IN";
+export const LOG_OUT = "LOG_OUT";
