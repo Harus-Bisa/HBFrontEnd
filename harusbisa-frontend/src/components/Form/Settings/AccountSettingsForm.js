7fd29a81ecca548ac26470754adb7a05cfea3a95
Danika Widjaja <danika.dew@utexas.edu>
2019-11-17 00:59:25 -0600

    account settings form impl init

---
+++ harusbisa-frontend/src/components/Form/Settings/AccountSettingsForm.js
@@ -0,0 +1,114 @@
+import React from "react";
+import {connect} from "react-redux";
+import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
+
+function AccountSettingsForm(props){
+    var [firstName, setFirstName] = React.useState(props.firstName);
+    var [lastName, setLastName] = React.useState(props.lastName);
+    var [school, setSchool] = React.useState(props.school);
+    var [email, setEmail] = React.useState(props.email);
+
+    const submitProfile = (event) =>{
+        event.preventDefault();
+        console.log("submitProfile")
+    }
+
+    if(props.loading){
+        return <p>Loading</p>
+    }
+    return(
+        <div>
+            <div className="content" style={{borderBottom:"2px solid #F4F4F4", paddingLeft: 0, paddingRight:0}}>
+                <div>
+                    <h5 style={{fontSize:'18px'}}>Basic</h5>
+                </div>
+                <div className="row justify-content-end">
+                    <div className="col-md-11">
+                        <Form onSubmit={submitProfile}>
+                            <FormGroup row>
+                                <Label sm={5}>Nama Depan</Label>
+                                <Col sm={7}>
+                                    <Input type="text" id="firstName" value={firstName} onChange={(event) => setFirstName(firstName = event.target.value) }/>
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup row>
+                                <Label sm={5}>Nama Belakang</Label>
+                                <Col sm={7}>
+                                    <Input type="text" id="lastName" value={lastName} onChange={(event) => setLastName(lastName = event.target.value)}/>
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup row>
+                                <Label sm={5}>Nama Universitas</Label>
+                                <Col sm={7}>
+                                    <Input type="text" id="school" value={school} onChange={(event) => setSchool(school= event.target.value)}/>
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup row>
+                                <Label sm={5}>Email</Label>
+                                <Col sm={7}>
+                                    <Input type="email" id="email" value={email} onChange={(event) => setEmail(email = event.target.value)}/>
+                                </Col>
+                            </FormGroup>
+
+                            <div className="row justify-content-end">
+                                <div className="col-sm-7">
+                                    <Button type="submit" className="neutral-button" style={{width:'100%'}}>Simpan perubahan</Button>
+                                </div>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="content" style={{paddingLeft: 0, paddingRight:0}}>
+                <h5 style={{fontSize:'18px'}}>Password</h5>
+                <div className="row justify-content-end">
+                    <div className="col-md-11">
+                        <p style={{marginBottom:'2rem'}}>Dengan membuat password memastkan anda dapat log in ke akun Harus Bisa anda</p>
+                        <Form onSubmit={submitProfile}>
+                            <FormGroup row>
+                                <Label sm={5}>Password Lama</Label>
+                                <Col sm={7}>
+                                    <Input type="text" id="oldPassword"/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label sm={5}>Password Baru</Label>
+                                <Col sm={7}>
+                                    <Input type="text" id="newPassword"/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Label sm={5}>Ulangi Password Baru</Label>
+                                <Col sm={7}>
+                                    <Input type="text" id="verifyNewPassword"/>
+                                </Col>
+                            </FormGroup>
+                            <div className="row justify-content-end">
+                                <div className="col-sm-7">
+                                    <Button type="submit" className="neutral-button" style={{width:'100%'}}>Simpan perubahan</Button>
+                                </div>
+                            </div>
+                        </Form>
+                    </div>
+                </div>
+            </div> 
+        </div>
+    )
+}
+
+function mapStateToProps(state){
+    console.log(state)
+    return{
+        firstName: state.firstName,
+        lastName: state.lastName,
+        email: state.email,
+        school: state.school,
+        loading: state.loading
+    }
+}
+
+export default connect(mapStateToProps)(AccountSettingsForm);
