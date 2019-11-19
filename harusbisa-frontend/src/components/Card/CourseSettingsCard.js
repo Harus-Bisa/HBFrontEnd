8e25088ca08f39bf58f7216b89dafd6f2383191f
Danika Widjaja <danika.dew@utexas.edu>
2019-11-19 13:18:49 -0600

    update

--- harusbisa-frontend/src/components/Card/CourseSettingsCard.js
+++ harusbisa-frontend/src/components/Card/CourseSettingsCard.js
@@ -4,15 +4,16 @@ import { Button } from "reactstrap";
 
 function CourseSettingsCard(props){
     return(
-        <div style={{margin:'15px 0'}}>
+        <div style={{margin:'30px 0px'}}>
             <div className="row">
                 <div className="col-8">
                     <h5>{props.course.course_name}</h5>
                     <p>{props.course.instructor}</p>
                     <p>{props.course.start_term} - {props.course.end_term}</p>
+                    <p>Kode Bergabung: {props.course.join_code}</p>
                 </div>
                 <div className="col-4">
-                    <Button className="neutral-button">Drop kelas</Button>
+                    <Button className="neutral-button" style={{width:'100%'}}>Drop kelas</Button>
                 </div>
             </div>
         </div>
