ff587ddd345febb28cf92b3bba28e9b88b0b9b3a
Danika Widjaja <danika.dew@utexas.edu>
2019-11-19 13:18:57 -0600

    adding arrows

--- harusbisa-frontend/src/pages/Settings/Settings.js
+++ harusbisa-frontend/src/pages/Settings/Settings.js
@@ -12,28 +12,45 @@ function Settings(props){
     if (props.loading){
         return null
     }
-    const buttonStyle = {
-        fontFamily: "ITC Avant Garde Gothic Std",
-        color: "black",
-        textAlign: "center"
+    const arrowUpStyle = {
+        width: 0, 
+        height: 0, 
+        borderLeft: "5px solid transparent",  
+        borderRight: "5px solid transparent",
+        borderBottom: "5px solid #f4f4f4",
+        fontsize: 0,
+        lineheight: 0,
     }
     return(
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-lg-6 col-md-8 col-sm">
                     <div className="content">
-
-                        <div className="row" style={{borderBottom:'4px solid #f4f4f4'}}>
-                            <div className="col-md-6">
-                                <h1>Settings</h1>
-                            </div>
-                            <div className="col-md-6" style={{display:'flex'}}>
-                                <div className="row" style={{margin: "auto"}}>
-                                    <div className="col-6">
-                                        <Button style={buttonStyle} className= "button" onClick={() => setSettingDisplay(settingDisplay=ACCOUNT)}>Akun</Button>
+                        <div style={{borderBottom:'4px solid #f4f4f4'}}>
+                            <div className="row" >
+                                <div className="col-md-6">
+                                    <h1 style={{margin:'0'}}>Settings</h1>
+                                </div>
+                                <div className="col-md-6" style={{display:'flex'}}>
+                                    <div className="row" style={{margin: "auto"}}>
+                                        <div className="col-6">
+                                            <Button className= "button settings-button" onClick={() => setSettingDisplay(settingDisplay=ACCOUNT)}>Akun</Button>
+                                        </div>
+                                        <div className="col-6">
+                                            <Button className= "button settings-button" onClick={() => setSettingDisplay(settingDisplay=COURSE)}>Kelas</Button>
+                                        </div>
                                     </div>
-                                    <div className="col-6">
-                                        <Button style={buttonStyle} className= "button" onClick={() => setSettingDisplay(settingDisplay=COURSE)}>Kelas</Button>
+                                </div>
+                            </div>
+                            <div className="row justify-content-end">
+                                <div className="col-md-6">
+                                    <div className="row" style={{margin: "auto"}}>
+                                        <div className="col-6">
+                                            <div style={{display:'flex', justifyContent:"center"}}><div style={(settingDisplay === ACCOUNT ? arrowUpStyle : {})} id={ACCOUNT}/></div>
+                                        </div>
+                                        <div className="col-6">
+                                            <div style={{display:'flex', justifyContent:"center"}}><div style={(settingDisplay === COURSE ? arrowUpStyle : {})} id={COURSE}/></div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
