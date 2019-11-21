25b5373102e5af109588b31fbaa046e34731fa0d
Danika Widjaja <danika.dew@utexas.edu>
2019-11-21 13:51:46 -0600

    prof course form edit

--- harusbisa-frontend/src/components/Form/CourseForm.js
+++ harusbisa-frontend/src/components/Form/CourseForm.js
@@ -1,6 +1,9 @@
 import React from "react";
 import {connect} from "react-redux";
 import { addCourse, editCourse } from "../../redux/actions";
+import { Form, FormGroup, Label, Input, FormFeedback, FormText, Col} from 'reactstrap';
+import { HB_YELLOW } from "../../css/constants/color";
+import { Button } from "@material-ui/core";
 
 function mapStateToProps(state, ownProps){
     if (ownProps.id){
@@ -41,8 +44,8 @@ function CourseForm(props){
         props.closePopup();
     }
     return(
-        <div>
-            <form onSubmit={submit}>
+        <div className="container-fluid">
+            {/* <form onSubmit={submit}>
                 <label>Course Name*</label>
                 <input type="text" value={name} onChange={(event) => setName(event.target.value)}></input>
                 <br/>
@@ -53,7 +56,52 @@ function CourseForm(props){
                 <input type="text" value={endDate} onChange={(event) => setEndDate(event.target.value)}></input>
                 <br/>
                 <button type="submit">Submit</button>
-            </form>
+            </form> */}
+            <div className="row" style={{borderBottom:'2px solid '+HB_YELLOW}}>
+                <div className="col-12" style={{display:'flex', justifyContent:'center', padding:'1rem 2rem'}}>
+                    <h3>Tambahkah Kelas Baru</h3>
+                </div>
+            </div>
+            <div className="content">
+                <Form onSubmit={submit}>
+                    <FormGroup row>
+                        <Label sm={5}>Nama Kelas</Label>
+                        <Col sm={7}>
+                            <Input type="text" value={name} placeholder={"Contoh: Biologi Molekuler Kelas A"} onChange={(event) => setName(event.target.value)}/>
+                            <FormText>Nama ini adalah nama yang akan dilihat siswa Anda.</FormText>
+                        </Col>
+                    </FormGroup>
+                    <FormGroup row>
+                        <Label sm={5}>Periode Kelas</Label>
+                        <Col sm={7}>
+                            <div className="row">
+                                <div className="col-5">
+                                    <Input type="select" name="select" id="exampleSelect">
+                                        <option>Juli 2019</option>
+                                        <option>Juli 2020</option>
+                                        <option>Juli 2021</option>
+                                        <option>Juli 2022</option>
+                                        <option>Juli 2023</option>
+                                    </Input>
+                                </div>
+                                <div className="col-2">
+                                    <p>-</p>
+                                </div>
+                                <div className="col-5">
+                                    <Input type="select" name="select" id="exampleSelect">
+                                        <option>Juli 2019</option>
+                                        <option>Juli 2020</option>
+                                        <option>Juli 2021</option>
+                                        <option>Juli 2022</option>
+                                        <option>Juli 2023</option>
+                                    </Input>
+                                </div>
+                            </div>
+                        </Col>
+                    </FormGroup>
+                    <Button className="prof-button" type="submit">Submit</Button>
+                </Form>
+            </div>
         </div>
     )
 }
--- harusbisa-frontend/src/css/constants/color.js
+++ harusbisa-frontend/src/css/constants/color.js
@@ -1,1 +1,2 @@
 export const HB_PURPLE = "#6311AB"
+export const HB_YELLOW = "#FFE01C"
