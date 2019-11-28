857f674ac9bcf57951b36f90692b1017b8343ab1
Danika Widjaja <danika.dew@utexas.edu>
2019-11-27 19:45:43 -0600

    quiz card css

--- harusbisa-frontend/src/components/Card/QuizCard.js
+++ harusbisa-frontend/src/components/Card/QuizCard.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Card, CardContent } from "@material-ui/core";
+import { Card, CardContent, Collapse, CardActions } from "@material-ui/core";
 import {connect} from "react-redux";
+import ButtonIcon from "../ButtonIcon/ButtonIcon";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function mapStateToProps(state, ownProps){
     var quizzes = state.selectedLecture.quizzes
@@ -10,23 +12,37 @@ function mapStateToProps(state, ownProps){
     }
 }
 function QuizCard(props){
+    var [expand, setExpand] = React.useState(false)
     const makeAnswers = () =>{
         var answers = []
         for (let i=0; i<props.quiz.answerOptions.length; i++){
-            answers.push(<div key={i}>{String.fromCharCode(i+65)}. {props.quiz.answerOptions[i]}</div>)
+            answers.push(<div key={i}><p>{String.fromCharCode(i+65)}. {props.quiz.answerOptions[i]}</p></div>)
         }
         return answers
     }
     return(
         <Card className="card">
             <CardContent>
-                <div>
-                    <p>{props.index + 1}.  {props.quiz.question}</p>
-                    <div>
-                        {makeAnswers()}
+                <div className="row">
+                    <div className="col-1">
+                        <p>{props.index + 1}</p>
+                    </div>
+                    <div className="col-9">
+                        <p>{props.quiz.question}</p>
+                    </div>
+                    <div className="col-2">
+                        <ButtonIcon/>
                     </div>
                 </div>
             </CardContent>
+            <Collapse in={expand}>
+                <div>
+                    {makeAnswers()}
+                </div>
+            </Collapse>
+            <CardActions>
+                <ButtonIcon icon={ExpandMoreIcon} onClick={() => setExpand(!expand)}/>
+            </CardActions>
         </Card>
     )
 }
